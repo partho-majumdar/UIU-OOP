ad49007da5f57723aff71cd1c21eb794e0e455eb
heipartho05 <heipartho72@gmail.com>
2023-05-29 11:52:17 +0600

    Q4 added

---
+++ src/Lab02/Solution/Q4.java
@@ -0,0 +1,15 @@
+package Lab02.Solution;
+
+public class Q4 {
+    public static void main(String[] args) {
+        float a = 9.5f;
+        float b = 4.5f;
+        float c = 2.5f;
+        int d = 3;
+        float e = 45.5f;
+        float f = 3.5f;
+
+        float ans = ((a * b) - (c * d)) / (e - f);
+        System.out.println(ans);
+    }
+}
