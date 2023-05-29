918af86402cd3da6d612179c63a69364d9e3f34d
heipartho05 <heipartho72@gmail.com>
2023-05-29 12:41:34 +0600

    Q9 added

--- src/Lab02/Solution/Q9.java
+++ src/Lab02/Solution/Q9.java
@@ -1,2 +1,15 @@
-package Lab02.Solution;public class Q9 {
+package Lab02.Solution;
+
+import java.util.Scanner;
+
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter value in celsius: ");
+        double celsius = input.nextDouble();
+
+        double fahrenheit = ((float) (9 / 5) * celsius) + 32;
+        System.out.println("Value in fahrenheit: " + fahrenheit);
+    }
 }
