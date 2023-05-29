310822e33f8267788735ac0c5d2871c55c96a322
heipartho05 <heipartho72@gmail.com>
2023-05-29 14:07:20 +0600

    Q14 added

--- src/Lab02/Solution/Q14.java
+++ src/Lab02/Solution/Q14.java
@@ -1,2 +1,40 @@
-package Lab02.Solution;public class Q14 {
+package Lab02.Solution;
+
+import java.util.Scanner;
+
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+//        System.out.print("Enter center(p, q) value of p: ");
+//        float p = input.nextFloat();
+//
+//        System.out.print("Enter center(p, q) value of q: ");
+//        float q = input.nextFloat();
+
+        float p = 0;
+        float q = 0;
+
+        System.out.print("Enter value of radius: ");
+        float radius = input.nextFloat();
+
+        System.out.print("Enter point(x, y) value of x: ");
+        float x = input.nextFloat();
+
+        System.out.print("Enter point(x, y) value of y: ");
+        float y = input.nextFloat();
+
+        double value_of_x = Math.pow((x - p), 2);
+        double value_of_y = Math.pow((y - q), 2);
+
+        double distance = Math.sqrt(value_of_x + value_of_y);
+
+        if (distance > radius) {
+            System.out.println("Point is outside the circle");
+        } else if (distance < radius) {
+            System.out.println("Point is inside the circle");
+        } else if (distance == radius) {
+            System.out.println("Point is in the circle");
+        }
+    }
 }
