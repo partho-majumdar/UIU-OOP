1955cb2a0099ce4b2ace5a79eadb557f407275a3
heipartho05 <heipartho72@gmail.com>
2023-05-29 11:59:37 +0600

    Q5 added

---
+++ src/Lab02/Solution/Q5.java
@@ -0,0 +1,15 @@
+package Lab02.Solution;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        float radius = 5.5f;
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+    }
+}
