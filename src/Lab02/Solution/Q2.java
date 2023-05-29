8299f49d4d90c3e7d7c203dd0e0015d7f081ab7c
heipartho05 <heipartho72@gmail.com>
2023-05-29 11:41:54 +0600

    Q2 added

---
+++ src/Lab02/Solution/Q2.java
@@ -0,0 +1,9 @@
+package Lab02.Solution;
+
+public class Q2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
