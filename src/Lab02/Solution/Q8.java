6dd242627968f6b42847bdcfe89a2bcb6323913a
heipartho05 <heipartho72@gmail.com>
2023-05-29 12:35:52 +0600

    Q8 added

---
+++ src/Lab02/Solution/Q8.java
@@ -0,0 +1,24 @@
+package Lab02.Solution;
+
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter runner runs in km: ");
+        float run_in_km = input.nextFloat();
+
+        System.out.print("Enter time in minute: ");
+        float time_in_min = input.nextFloat();
+
+        System.out.print("Enter time in second: ");
+        float time_in_sec = input.nextFloat();
+
+        double miles = run_in_km / 1.6f;
+        double time_in_hour = (float) ((time_in_min * 60) + time_in_sec) / 3600.0f;
+
+        double miles_per_hour = miles / time_in_hour;
+        System.out.println("Miles per hour: " + miles_per_hour);
+    }
+}
