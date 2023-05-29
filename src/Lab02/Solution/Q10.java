277aff042a10070a2310a8381f4da81bb6131601
heipartho05 <heipartho72@gmail.com>
2023-05-29 12:46:33 +0600

    Q10 added

---
+++ src/Lab02/Solution/Q10.java
@@ -0,0 +1,17 @@
+package Lab02.Solution;
+
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0 && number % 3 == 0) {
+            System.out.println("False");
+        } else if (number % 2 == 0 || number % 3 == 0) {
+            System.out.println("True");
+        }
+    }
+}
