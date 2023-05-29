f42aa61f4a50cb69bc89ee8077668adba0ecabbd
heipartho05 <heipartho72@gmail.com>
2023-05-29 12:00:37 +0600

    Q6 added

---
+++ src/Lab02/Solution/Q6.java
@@ -0,0 +1,17 @@
+package Lab02.Solution;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter radius: ");
+        float radius = input.nextFloat();
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+    }
+}
