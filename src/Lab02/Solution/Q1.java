f1be377e7eea019780f94cb5480a69e819b89592
heipartho05 <heipartho72@gmail.com>
2023-05-29 11:36:32 +0600

    Q1 added

--- src/Lab02/Solution/Q1.java
+++ src/Lab02/Solution/Q1.java
@@ -2,6 +2,8 @@ package Lab02.Solution;
 
 public class Q1 {
     public static void main(String[] args) {
-        System.out.println(4);
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
     }
 }
