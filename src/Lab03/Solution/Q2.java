b16c0e0649c1164bc507610b6ee99acd85daadaa
heipartho05 <heipartho72@gmail.com>
2023-05-30 23:51:24 +0600

    q2 added

--- src/Lab03/Solution/Q2.java
+++ src/Lab03/Solution/Q2.java
@@ -1,2 +1,17 @@
-package Lab03.Solution;public class Q2 {
+package Lab03.Solution;
+
+import javax.swing.*;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog("Enter first number: "));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog("Enter second number: "));
+
+        while (n2 != 0) {
+            int rem = n1 % n2;
+            n1 = n2;
+            n2 = rem;
+        }
+        JOptionPane.showMessageDialog(null, "GCD: " + n1);
+    }
 }
