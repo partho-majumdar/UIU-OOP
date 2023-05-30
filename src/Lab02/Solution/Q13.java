f18dfd7184bfffa10a07ab7ca21b03e8efde8c76
heipartho05 <heipartho72@gmail.com>
2023-05-30 23:50:20 +0600

    q13 modify

--- src/Lab02/Solution/Q13.java
+++ src/Lab02/Solution/Q13.java
@@ -4,16 +4,15 @@ import javax.swing.*;
 
 public class Q13 {
     public static void main(String[] args) {
-
         int input1 = Integer.parseInt(JOptionPane.showInputDialog("Enter 0 (convert USD to BDT) and 1 (convert BDT to USD): "));
 
         if (input1 == 0) {
             float val = Float.parseFloat(JOptionPane.showInputDialog("Enter USD: "));
-            float ans = val * 90;
+            float ans = (float) (val * 107.27);
             JOptionPane.showMessageDialog(null, "BDT: " + ans + " TK");
         } else if (input1 == 1) {
             float val1 = Float.parseFloat(JOptionPane.showInputDialog("Enter BDT: "));
-            float ans1 = val1 / 90;
+            float ans1 = (float) (val1 / 107.27);
             JOptionPane.showMessageDialog(null, "USD: " + ans1 + " $");
         } else {
             JOptionPane.showMessageDialog(null, "Enter valid input.");
