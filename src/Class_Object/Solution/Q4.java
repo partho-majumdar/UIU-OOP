c5f0737b2ce207103285b93b823fe5448430f478
heipartho05 <heipartho72@gmail.com>
2023-06-06 22:27:29 +0600

    q4 added

---
+++ src/Class_Object/Solution/Q4.java
@@ -0,0 +1,31 @@
+package Class_Object.Solution;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Triangle tri = new Triangle();
+        tri.a = 3;
+        tri.b = 4;
+        tri.c = 5;
+
+        System.out.println("Area: " + tri.getArea());
+        System.out.println("Perimeter: " + tri.getPerimeter());
+    }
+}
+
+class Triangle {
+    float a, b, c;
+
+    double getPerimeter() {
+        return (a + b + c) / 2;
+    }
+
+    double getArea() {
+        double s = getPerimeter();
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
+/*
+Write a program to print the area and perimeter of a
+triangle having sides of 3, 4 and 5 units by creating a class
+named 'Triangle' without any parameter in its constructor.
+ */
