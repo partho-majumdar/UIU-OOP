1f44063de7085ea99f58f16dc28523713a216432
heipartho05 <heipartho72@gmail.com>
2023-06-06 22:52:14 +0600

    q4 modify

--- src/Class_Object/Solution/Q4.java
+++ src/Class_Object/Solution/Q4.java
@@ -24,6 +24,7 @@ class Triangle {
         return Math.sqrt(s * (s - a) * (s - b) * (s - c));
     }
 }
+
 /*
 Write a program to print the area and perimeter of a
 triangle having sides of 3, 4 and 5 units by creating a class
