66aa3ec1d5c6698bd2c50d8cc364d0291e41323f
heipartho05 <heipartho72@gmail.com>
2023-06-06 21:44:30 +0600

    q2 added

--- src/Class_Object/Solution/Q2.java
+++ src/Class_Object/Solution/Q2.java
@@ -1,2 +1,23 @@
-package Class_Object.Solution;public class Q2 {
+package Class_Object.Solution;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Student stu = new Student();
+        stu.name = "John";
+        stu.roll_no = 2;
+
+        System.out.println("Name: " + stu.name + ", Roll: " + stu.roll_no);
+    }
 }
+
+
+class Student {
+    String name;
+    int roll_no;
+}
+
+/*
+ * Create a class named 'Student' with String variable 'name' and integer
+ * variable 'roll_no'. Assign the value of roll_no as '2' and that of name as
+ * "John" by creating an object of the class Student.
+ */
