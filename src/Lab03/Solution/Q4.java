2fef513c187d0d658861e1e0dc6b1160f974bf3a
heipartho05 <heipartho72@gmail.com>
2023-06-09 17:46:50 +0600

    q4 added

--- src/Lab03/Solution/Q4.java
+++ src/Lab03/Solution/Q4.java
@@ -1,2 +1,27 @@
-package Lab03.Solution;public class Q4 {
+package Lab03.Solution;
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+
+        int[] occurrences = new int[101];
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        do {
+            System.out.print("Enter number form 1-100: ");
+            number = input.nextInt();
+
+            if (number >= 1 && number <= 100) {
+                occurrences[number]++;
+            }
+        } while (number != 0);
+
+        for (int i = 1; i <= 100; i++) {
+            if (occurrences[i] > 0) {
+                System.out.printf("%d occur %d times\n", i, occurrences[i]);
+            }
+        }
+    }
 }
