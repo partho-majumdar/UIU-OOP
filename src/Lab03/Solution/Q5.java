6904b20e91dc5245aa525d136da0a9d0f3fdfa47
heipartho05 <heipartho72@gmail.com>
2023-06-09 18:09:07 +0600

    q5 added

---
+++ src/Lab03/Solution/Q5.java
@@ -0,0 +1,30 @@
+package Lab03.Solution;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any string: ");
+        String str = input.nextLine();
+        int len = str.length();
+        int ans = isPalindrome(str, len);
+
+        if (ans == 1) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+
+    static int isPalindrome(String word, int len) {
+        int flag = 1;
+        for (int i = 0; i < len; i++) {
+            if (word.charAt(i) != word.charAt(len - i - 1)) {
+                flag = 0;
+                break;
+            }
+        }
+        return flag;
+    }
+}
