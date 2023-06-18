11c06a3a56c708f744b878f5fb8cbf4ea826f193
heipartho05 <heipartho72@gmail.com>
2023-06-19 02:30:38 +0600

    q6 modify

--- src/Lab03/Solution/Q6.java
+++ src/Lab03/Solution/Q6.java
@@ -22,6 +22,7 @@ public class Q6 {
         for (int i = 0; i < count; i++) {
             System.out.print(arr[i] + " ");
         }
+        input.close();
     }
 
     static boolean isDistinct(int[] arr2, int count, int num) {
@@ -35,49 +36,49 @@ public class Q6 {
 }
 
 /*
----------------- Rule 02 -------------
-
- static void removeDuplicate(int[] arr, int len) {
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (arr[i] == arr[j]) {
-                    for (int k = j; k < len - 1; k++) {
-                        arr[k] = arr[k + 1];
-                    }
-                    len--;
-                    j--;
-                }
-            }
-        }
-
-        int count = 0;
-        for (int i = 0; i < len; i++) {
-            System.out.print(arr[i] + " ");
-            count++;
-        }
-        System.out.println();
-        System.out.println("Number of distinct element: " + count);
-*/
+ * ---------------- Rule 02 -------------
+ * 
+ * static void removeDuplicate(int[] arr, int len) {
+ * for (int i = 0; i < len - 1; i++) {
+ * for (int j = i + 1; j < len; j++) {
+ * if (arr[i] == arr[j]) {
+ * for (int k = j; k < len - 1; k++) {
+ * arr[k] = arr[k + 1];
+ * }
+ * len--;
+ * j--;
+ * }
+ * }
+ * }
+ * 
+ * int count = 0;
+ * for (int i = 0; i < len; i++) {
+ * System.out.print(arr[i] + " ");
+ * count++;
+ * }
+ * System.out.println();
+ * System.out.println("Number of distinct element: " + count);
+ */
 
 /*
---------------Rule 03 --------------
-    static ArrayList<Integer> isDistinct(int[] arr, int length) {
-        ArrayList<Integer> unique = new ArrayList<>();
-
-        for (int i = 0; i < length; i++) {
-            boolean isDuplicate = false;
-            for (int j = 0; j < i; j++) {
-                if (arr[i] == arr[j]) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if (!isDuplicate) {
-                unique.add(arr[i]);
-            }
-        }
-        return unique;
-    }
-}
-
+ * --------------Rule 03 --------------
+ * static ArrayList<Integer> isDistinct(int[] arr, int length) {
+ * ArrayList<Integer> unique = new ArrayList<>();
+ * 
+ * for (int i = 0; i < length; i++) {
+ * boolean isDuplicate = false;
+ * for (int j = 0; j < i; j++) {
+ * if (arr[i] == arr[j]) {
+ * isDuplicate = true;
+ * break;
+ * }
+ * }
+ * if (!isDuplicate) {
+ * unique.add(arr[i]);
+ * }
+ * }
+ * return unique;
+ * }
+ * }
+ * 
  */
