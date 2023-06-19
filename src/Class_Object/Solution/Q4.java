608ed62cec21c2aaae15b616ac50a50507b3186c
heipartho05 <heipartho72@gmail.com>
2023-06-20 02:54:11 +0600

    q4 modify

--- src/Class_Object/Solution/Q4.java
+++ src/Class_Object/Solution/Q4.java
@@ -26,7 +26,7 @@ class Triangle {
 }
 
 /*
-Write a program to print the area and perimeter of a
-triangle having sides of 3, 4 and 5 units by creating a class
-named 'Triangle' without any parameter in its constructor.
+ * Write a program to print the area and perimeter of a
+ * triangle having sides of 3, 4 and 5 units by creating a class
+ * named 'Triangle' without any parameter in its constructor.
  */
