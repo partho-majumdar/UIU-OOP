ce9b52d2dd86934567064fb1b786b8951ec0e357
heipartho05 <heipartho72@gmail.com>
2023-06-23 15:57:56 +0600

    main modify

--- src/Lab04/ClassObjectBasic/Main.java
+++ src/Lab04/ClassObjectBasic/Main.java
@@ -1,8 +1,61 @@
+package Lab04.ClassObjectBasic;
+
+import Lab04.ClassObjectBasic.IcecreamProject.src1.Icecream;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static ArrayList<Icecream> icecreams = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your input: ");
+
+        while (true) {
+            System.out.println("0. Exit\n1. Input");
+            int x = input.nextInt();
+            if (x == 0) {
+                break;
+            }
+
+            System.out.print("Enter the flavor: ");
+            String type = input.next();
+            System.out.print("Company: ");
+            String company = input.next();
+            System.out.print("Price: ");
+            double price = input.nextDouble();
+
+            Icecream other = new Icecream(type, company, price);
+
+            boolean flag = true;
+            for (Icecream e : icecreams) {
+                if (other.equals(e)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                icecreams.add(other);
+            } else {
+                System.out.println("Already exists!");
+            }
+        }
+
+        for (Icecream e : icecreams) {
+            System.out.println(e.toString());
+        }
+
+        searchByCompany("Igloo");
+        sortByPrice();
+
+        for (Icecream e : icecreams) {
+            System.out.println(e.toString());
+        }
+        input.close();
+    }
 
     public static void searchByCompany(String company) {
         for (Icecream e : icecreams) {
@@ -16,56 +69,11 @@ public class Main {
         for (int i = 0; i < icecreams.size(); i++) {
             for (int j = i + 1; j < icecreams.size(); j++) {
                 if (icecreams.get(i).getIcecreamPrice() > icecreams.get(j).getIcecreamPrice()) {
-                    Icecream I = icecreams.get(i);
+                    Icecream other = icecreams.get(i);
                     icecreams.set(i, icecreams.get(j));
-                    icecreams.set(j, I);
+                    icecreams.set(j, other);
                 }
             }
         }
-    }
-
-    public static void main(String[] args) {
-
-        // Icecream I1 = new Icecream("Vanilla", "Polar", 250.0);
-        // System.out.println(I1.toString());
-        // I1.setIcecreamType("Chocolate");
-        // System.out.println(I1.toString());
-        // Icecream I2 = new Icecream("Vanilla", "Polar", 250.0);
-        // I1.equals(I2);
-
-        Scanner input = new Scanner(System.in);
-        while (true) {
-            System.out.println("0. Exit\n1. Input");
-            int x = input.nextInt();
-            if (x == 0)
-                break;
-            System.out.println("Enter the flavor: ");
-            String type = input.next();
-            System.out.println("Company: ");
-            String company = input.next();
-            System.out.println("Price: ");
-            double price = input.nextDouble();
-            Icecream I = new Icecream(type, company, price);
-            boolean flag = true;
-            for (Icecream e : icecreams) {
-                if (I.equals(e)) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag)
-                icecreams.add(I);
-            else
-                System.out.println("Already exists!");
-        }
-        for (Icecream e : icecreams) {
-            System.out.println(e.toString());
-        }
-        searchByCompany("Igloo");
-        sortByPrice();
-        for (Icecream e : icecreams) {
-            System.out.println(e.toString());
-        }
-        input.close();
     }
 }
