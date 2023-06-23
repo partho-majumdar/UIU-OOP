6845820976566306d751f54c65cbf661094348c2
heipartho05 <heipartho72@gmail.com>
2023-06-23 15:13:35 +0600

    q12 modify

--- src/Lab03/Solution/Q12.java
+++ src/Lab03/Solution/Q12.java
@@ -16,9 +16,9 @@ public class Q12 {
 
         int flag = 0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
+                if (arr[i] == arr[j] && i != j) {
                     flag = 1;
                     break;
                 }
