22cdba09cd5571deb710c70b4bb61144c70e4f5b
heipartho05 <heipartho72@gmail.com>
2023-08-18 17:34:34 +0600

    checking account class added

--- src/Lab07_08/Solution/CheckingAccount.java
+++ src/Lab07_08/Solution/CheckingAccount.java
@@ -1,2 +1,18 @@
-package Lab07_08.Solution;public class CheckingAccount {
+package Lab07_08.Solution;
+
+public class CheckingAccount extends Account {
+    private double overdraftLimit;
+
+    public CheckingAccount(int id, double balance, double annualInterestRate, double overdraftLimit) {
+        super(id, balance, annualInterestRate);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
 }
