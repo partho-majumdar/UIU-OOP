76efaa73e938eac78b4a0d71f6520d6441d9fb80
heipartho05 <heipartho72@gmail.com>
2023-08-18 17:35:07 +0600

    savings account class added

--- src/Lab07_08/Solution/SavingsAccount.java
+++ src/Lab07_08/Solution/SavingsAccount.java
@@ -1,2 +1,42 @@
-package Lab07_08.Solution;public class SavingsAccount {
+package Lab07_08.Solution;
+
+import java.util.Calendar;
+
+public class SavingsAccount extends Account {
+    private String creditCardNumber;
+    private Calendar cardExpiryDate;
+
+    public SavingsAccount(int id, double balance, double annualInterestRate) {
+        super(id, balance, annualInterestRate);
+        cardExpiryDate = Calendar.getInstance();
+        creditCardNumber = generateCardNumber();
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public Calendar getCardExpiryDate() {
+        return cardExpiryDate;
+    }
+
+    public void setCardExpiryDate(Calendar cardExpiryDate) {
+        this.cardExpiryDate = cardExpiryDate;
+    }
+
+    public double getCreditBalance() {
+        return getBalance() * 3.0;
+    }
+
+    public String generateCardNumber() {
+        for (int i = 0; i < 16; i++) {
+            int digit = (int) (Math.random() * 10);
+            creditCardNumber += digit;
+        }
+        return creditCardNumber;
+    }
 }
