f4c78ad115b2a5d377b947314ffe29ede5fbbb0a
heipartho05 <heipartho72@gmail.com>
2023-08-25 10:04:51 +0600

    public university class added

--- src/OOP_4_Pillars/Solution/Solution_01/PublicUniversity.java
+++ src/OOP_4_Pillars/Solution/Solution_01/PublicUniversity.java
@@ -1,2 +1,40 @@
-package OOP_4_Pillars.Solution.Solution_01;public class PublicUniversity {
+package OOP_4_Pillars.Solution.Solution_01;
+
+public class PublicUniversity extends University {
+    private int numberOfHalls;
+    private boolean politicsAllowed;
+
+    public PublicUniversity(String name, String address, int dateEstablished, int numberOfDepartments,
+                            int numberOfHalls, boolean politicsAllowed) {
+        super(name, address, dateEstablished, numberOfDepartments);
+        this.numberOfHalls = numberOfHalls;
+        this.politicsAllowed = politicsAllowed;
+    }
+
+    public int getNumberOfHalls() {
+        return numberOfHalls;
+    }
+
+    public void setNumberOfHalls(int numberOfHalls) {
+        this.numberOfHalls = numberOfHalls;
+    }
+
+    public void setPoliticsAllowed(boolean politicsAllowed) {
+        this.politicsAllowed = politicsAllowed;
+    }
+
+    @Override
+    public boolean isUgcApproved() {
+        return false;
+    }
+
+    @Override
+    public boolean isPoliticsAllowed() {
+        return politicsAllowed;
+    }
+
+    @Override
+    public String toString() {
+        return "Public " + super.toString() + " [numberOfHalls=" + numberOfHalls + ", politicsAllowed=" + politicsAllowed + "]";
+    }
 }
