91a07ec0ead88f1f4e4151f80420852a24b8ef46
heipartho05 <heipartho72@gmail.com>
2023-08-25 14:56:57 +0600

    full time employee class added

--- src/OOP_4_Pillars/Solution/Solution_03/FulltimeEmployee.java
+++ src/OOP_4_Pillars/Solution/Solution_03/FulltimeEmployee.java
@@ -1,2 +1,30 @@
-package OOP_4_Pillars.Solution.Solution_03;public class FulltimeEmployee {
+package OOP_4_Pillars.Solution.Solution_03;
+
+public class FulltimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FulltimeEmployee(String name, int id, int day, int month, int year, double monthlySalary) {
+        super(name, id, day, month, year);
+        this.monthlySalary = monthlySalary;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double getSalary() {
+        return getMonthlySalary();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nFull-timeEmployee{" +
+                "monthlySalary=" + monthlySalary +
+                '}';
+    }
 }
