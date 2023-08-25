e5883a8809909321116d884b3deec708d874c377
heipartho05 <heipartho72@gmail.com>
2023-08-25 11:57:05 +0600

    area class added

--- src/Lab09/Solution/Q1/Area.java
+++ src/Lab09/Solution/Q1/Area.java
@@ -1,2 +1,19 @@
-package Lab09.Solution.Q1;public class Area {
+package Lab09.Solution.Q1;
+
+public class Area extends Shape {
+
+    @Override
+    public void RectangleArea(double length, double width) {
+        super.setArea(length * width);
+    }
+
+    @Override
+    public void SquareArea(double side) {
+        super.setArea(side * side);
+    }
+
+    @Override
+    public void CircleArea(double radius) {
+        super.setArea(Math.PI * radius * radius);
+    }
 }
