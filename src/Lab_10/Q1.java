d6d226f64fd6faaa755aef6bc3b2881daa522f2b
heipartho05 <heipartho72@gmail.com>
2023-08-25 11:58:44 +0600

    q1 added

--- src/Lab_10/Q1.java
+++ src/Lab_10/Q1.java
@@ -1,2 +1,40 @@
-package Lab_10;public class Q1 {
+package Lab_10;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Q1 {
+    public static void main(String[] args) {
+        try {
+            FileWriter fw = new FileWriter("test.txt");
+            BufferedWriter w = new BufferedWriter(fw);
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < 100; i++) {
+                int random = (int) (Math.random() * 100);
+                if (i == 0) {
+                    list.add(random);
+                } else {
+                    if (list.contains(random)) {
+                        i--;
+                    } else {
+                        list.add(random);
+                    }
+                }
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                w.write(list.get(i) + "\n");
+            }
+
+            w.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+// Generate random 100 number and write it in file
